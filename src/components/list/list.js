5afdd5ad5d075774e4035b3f8f7177dee26c7eec
W-ill-c <williamcollett36@gmail.com>
2023-03-29 13:05:43 +0100

    updated button functionality and styling and added loading section

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,19 +1,19 @@
 import {React, useEffect, useState} from "react";
 import Button from 'react-bootstrap/Button';
+import { FaSpinner } from "react-icons/fa";
 
 export default function WholeList(){
 
-    const [data, setData] = useState([]);
-
-    const [showFurtherInfo, setShowFurtherInfo] = useState(false);
+    const [schoolData, setSchoolData] = useState([]);
 
     useEffect(()=>{
         let collectData = async () => {
             try{
                 let data = await fetch("http://propertydog.co.uk/schools.json");
-                let schoolData = await data.json();
-                console.log(schoolData);
-                setData(data);
+                let fetchedData = await data.json();
+                console.log(typeof fetchedData);
+                console.log(fetchedData)
+                setSchoolData(fetchedData);
             } catch(err){
                 console.log(err)
             }
@@ -23,28 +23,58 @@ export default function WholeList(){
 
     // useEffect runs when the component mounts and either when the component rerenders or when the second argument changes. As the empty array never changes, the data is fetched once.
 
+    const openExtraInfo = (event)=>{
+        event.target.parentElement.parentElement.children[1].style.display="block"
+        event.target.style.opacity=0
+    }
+    const closeExtraInfo = (event)=>{
+        event.target.parentElement.parentElement.children[1].style.display="none"
+        event.target.style.opacity=0
+    }
+
     return(
         <section className="wholeList">
-            <div className="indiviudalSchool">
-                <div className="schoolInfo">
-                    <h4>School Name:</h4>
-                    <h5>School Postcode:</h5>
-                    <Button variant="primary" id="viewMoreButton" onClick={()=>{setShowFurtherInfo(!showFurtherInfo)}}>View Further Information</Button>{' '}
-                </div>
-
-                {/* The onClick event for the button inverts the value of the showFurtherInfo state*/}
-
-                {/* The && operator allows me to conditionally render the following div depending on the booleon value of the showFurtherInfo state  */}
-
-                {showFurtherInfo && 
-                    <div className="furtherSchoolInfo">
-                        <h5>Full Address:</h5>
-                        <h5>Education Stage:</h5>
-                        <h5>School Website:</h5>
-                        <h5>School Capacity:</h5>
-                    </div>
-                }
-            </div>
+            {(schoolData.length > 0) ?
+                schoolData.map( school => {
+                    return(
+                        <div className="indiviudalSchool" key={school.URN}>
+                            <div className="schoolInfo">
+                                <h4>School Name: {school.EstablishmentName}</h4>
+                                <h5>School Postcode: {school.Postcode}</h5>
+                                <Button variant="primary" id="viewMoreButton" onClick={
+                                    (e)=>{
+                                        openExtraInfo(e);
+                                        setTimeout(()=>{
+                                            e.target.parentElement.children[3].style.display="block"
+                                            e.target.parentElement.children[3].style.opacity=1
+                                            e.target.style.display="none"
+                                        }, 1000)
+                                    }
+                                }>View Further Information</Button>{' '}
+                                <Button variant="danger" id="viewLessButton" onClick={
+                                    (e)=>{
+                                        closeExtraInfo(e);
+                                        setTimeout(()=>{
+                                            e.target.parentElement.children[2].style.display="block"
+                                            e.target.parentElement.children[2].style.opacity=1
+                                            e.target.style.display="none"
+                                        }, 1000)
+                                    }
+                                }>Close Further Information</Button>{' '}
+                            </div>
+                            
+                            <div className="furtherSchoolInfo">
+                                <h5>Full Address:</h5>
+                                <h5>Education Stage:</h5>
+                                <h5>School Website:</h5>
+                                <h5>School Capacity:</h5>
+                            </div>
+                            
+                        </div>
+                    )
+                }) :
+                <FaSpinner id="spinner"/>
+            } 
         </section>
     );
 }
