0c408378b0ef8dfc54e6802bb435d04a3370b45f
W-ill-c <williamcollett36@gmail.com>
2023-03-30 10:54:39 +0100

    Completed header branch and scss

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import WholeList from './components/list/list';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/list/listStyles.css'
+import HeaderSection from './components/header/header'
+import './components/header/headerStyles.css'
 
 function App() {
   return (
     <section className="App">
+      <HeaderSection />
       <WholeList />
     </section>
   );
